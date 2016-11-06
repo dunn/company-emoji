3dad255d6928e28e7a700d8cbac060f87d43d25e
Alex Dunn <dunn.alex@gmail.com>
2016-11-05 21:38:10 -0700

    v2.4.0

--- company-emoji.el
+++ company-emoji.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alex Dunn <dunn.alex@gmail.com>
 ;; URL: https://github.com/dunn/company-emoji.git
-;; Version: 2.3.0
+;; Version: 2.4.0
 ;; Package-Requires: ((cl-lib "0.5") (company "0.8.0"))
 ;; Keywords: emoji company
 ;; Prefix: company-emoji
@@ -117,7 +117,7 @@
 
 ;; version string
 
-(defconst company-emoji-version "2.3.0")
+(defconst company-emoji-version "2.4.0")
 
 ;; customize
 
