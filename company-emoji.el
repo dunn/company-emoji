514b33c29ff384188e90aceceabf305205837081
Alexandra Dunn <dunn.alex@gmail.com>
2020-10-19 22:53:43 -0700

    v2.6.0

--- company-emoji.el
+++ company-emoji.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alex Dunn <dunn.alex@gmail.com>
 ;; URL: https://github.com/dunn/company-emoji.git
-;; Version: 2.5.2
+;; Version: 2.6.0
 ;; Package-Requires: ((cl-lib "0.5") (company "0.8.0"))
 ;; Keywords: emoji company
 ;; Prefix: company-emoji
@@ -117,7 +117,7 @@
 (require 'company)
 (require 'company-emoji-list)
 
-(defconst company-emoji-version "2.5.1"
+(defconst company-emoji-version "2.6.0"
   "Current version of company-emoji.")
 
 (defconst company-emojis (company-emoji-list-create)
